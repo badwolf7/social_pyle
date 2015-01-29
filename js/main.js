9599357d73f81538331f9c45980f495e1b3295a8
Holly Springsteen <hhspringsteen@gmail.com>
2015-01-29 16:48:17 -0500

    pricing cost toggle

--- js/main.js
+++ js/main.js
@@ -46,77 +46,95 @@ window.onload = function(){
 
 	//////////////////////// HOW IT WORKS
 	/////////////////////////////////////
-	function sideScroll(counter, margin){
-		perc = margin+"%";
-		$('.hiw section').css({
-			'margin-left':perc
+	if($('main').hasClass('landing')){
+		function sideScroll(counter, margin){
+			perc = margin+"%";
+			$('.hiw section').css({
+				'margin-left':perc
+			});
+		}
+		var counter = 0;
+		var margin = 0;
+		
+		setInterval(function(){
+			if(counter<4 && counter>0 && margin > -301){
+				margin -= 100;
+			}else if(counter>=4 && counter<7 && margin > -301){
+				margin += 100;
+			}else{
+				counter = 0;
+				margin = 0;
+			}
+
+			switch(margin){
+				case 0:
+					$('.hiw ul li').removeClass('active');
+					$('.hiw ul li:nth-child(1)').addClass('active');
+					break;
+				case -100:
+					$('.hiw ul li').removeClass('active');
+					$('.hiw ul li:nth-child(2)').addClass('active');
+					break;
+				case -200:
+					$('.hiw ul li').removeClass('active');
+					$('.hiw ul li:nth-child(3)').addClass('active');
+					break;
+				case -300:
+					$('.hiw ul li').removeClass('active');
+					$('.hiw ul li:nth-child(4)').addClass('active');
+					break;
+			}
+
+			console.log("counter: "+counter+" margin: "+margin);
+
+			sideScroll(counter, margin);
+			counter++;
+		}, 8000);
+
+		$('.hiw ul li:nth-child(1)').click(function(){
+			$('.hiw ul li').removeClass('active');
+			$(this).addClass('active')
+			margin = 0;
+			counter = 1;
+			sideScroll(counter, margin);
+		});
+		$('.hiw ul li:nth-child(2)').click(function(){
+			$('.hiw ul li').removeClass('active');
+			$(this).addClass('active')
+			margin = -100;
+			counter = 2;
+			sideScroll(counter, margin);
+		});
+		$('.hiw ul li:nth-child(3)').click(function(){
+			$('.hiw ul li').removeClass('active');
+			$(this).addClass('active')
+			margin = -200;
+			counter = 3;
+			sideScroll(counter, margin);
+		});
+		$('.hiw ul li:nth-child(4)').click(function(){
+			$('.hiw ul li').removeClass('active');
+			$(this).addClass('active')
+			margin = -300;
+			counter = 4;
+			sideScroll(counter, margin);
 		});
 	}
-	var counter = 0;
-	var margin = 0;
-	
-	setInterval(function(){
-		if(counter<4 && counter>0 && margin > -301){
-			margin -= 100;
-		}else if(counter>=4 && counter<7 && margin > -301){
-			margin += 100;
+
+
+	///////////////////////////// PRICING
+	/////////////////////////////////////
+	$('.annually').hide();
+	$('.cost-toggle').click(function(){
+		if($(this).siblings('.annually').css('display') == 'none'){
+			$(this).siblings('.monthly').hide();
+			$(this).siblings('.annually').show();
+			$(this).text('View Monthly')
 		}else{
-			counter = 0;
-			margin = 0;
+			$(this).siblings('.annually').hide();
+			$(this).siblings('.monthly').show();
+			$(this).text('View Annual')
 		}
-
-		switch(margin){
-			case 0:
-				$('.hiw ul li').removeClass('active');
-				$('.hiw ul li:nth-child(1)').addClass('active');
-				break;
-			case -100:
-				$('.hiw ul li').removeClass('active');
-				$('.hiw ul li:nth-child(2)').addClass('active');
-				break;
-			case -200:
-				$('.hiw ul li').removeClass('active');
-				$('.hiw ul li:nth-child(3)').addClass('active');
-				break;
-			case -300:
-				$('.hiw ul li').removeClass('active');
-				$('.hiw ul li:nth-child(4)').addClass('active');
-				break;
-		}
-
-		console.log("counter: "+counter+" margin: "+margin);
-
-		sideScroll(counter, margin);
-		counter++;
-	}, 8000);
-
-	$('.hiw ul li:nth-child(1)').click(function(){
-		$('.hiw ul li').removeClass('active');
-		$(this).addClass('active')
-		margin = 0;
-		counter = 1;
-		sideScroll(counter, margin);
-	});
-	$('.hiw ul li:nth-child(2)').click(function(){
-		$('.hiw ul li').removeClass('active');
-		$(this).addClass('active')
-		margin = -100;
-		counter = 2;
-		sideScroll(counter, margin);
-	});
-	$('.hiw ul li:nth-child(3)').click(function(){
-		$('.hiw ul li').removeClass('active');
-		$(this).addClass('active')
-		margin = -200;
-		counter = 3;
-		sideScroll(counter, margin);
-	});
-	$('.hiw ul li:nth-child(4)').click(function(){
-		$('.hiw ul li').removeClass('active');
-		$(this).addClass('active')
-		margin = -300;
-		counter = 4;
-		sideScroll(counter, margin);
 	});
 
 
