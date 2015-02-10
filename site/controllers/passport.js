6a7ef7c82253f038a5faf1e850eb42a226fe5df2
Holly Springsteen <hhspringsteen@gmail.com>
2015-02-10 16:21:05 -0500

    update twitter request
    use npm twitter instead of passport-twitter
       —> Easier for requesting data

--- site/controllers/passport.js
+++ site/controllers/passport.js
@@ -1,7 +1,10 @@
-//	Passport requires for FB login
+//	Passport
 var passport = require('passport');
+//  Passport Facebook
 var FacebookStrategy = require('passport-facebook').Strategy;
-var TwitterStrategy = require('passport-twitter').Strategy;
+//  Passport Twitter
+var Twitter = require('twitter');
+//  Passport Google
 var GooglePlusStrategy = require('passport-google-oauth').OAuthStrategy;
 
 //	Configure FB connect method
@@ -11,6 +14,9 @@ var FACEBOOK_APP_SECRET = 'f8478c7f34e451b27e3b1c118437ff8a';
 // Twitter App Data
 var TWITTER_CONSUMER_KEY = '9RZksOa8esVXKLytGQY3cXScJ';
 var TWITTER_CONSUMER_SECRET = 'ksc0TgxpwwP7xzsKAofoMCAiUc5TFGVk8CEFe5mFRSi2CShDqk';
+var TWITTER_ACCESS_TOKEN = '2863998855-tt2FzUqp4Xr0JHCNYfAM9siWAH8Y5dmSFZz8nk9';
+var TWITTER_ACCESS_TOKEN_SECRET = 'lJn5CeuGWDdDJzmXkQgzpv7dgEfLwF0qtiUchhuAnHNJK';
+var TWITTER_OWNER_ID = '2863998855';
 
 // Google App Data
 var GOOGLE_CLIENT_ID = '1044666733227-4u3rtup7s601l651q0ntti5itnpfr1h0.apps.googleusercontent.com';
@@ -64,47 +70,26 @@ module.exports = function(){
 				});
 		}
 	));
+	
+	//  Twitter
+	var client = new Twitter({
+		consumer_key: TWITTER_CONSUMER_KEY,
+		consumer_secret: TWITTER_CONSUMER_SECRET,
+		access_token_key: TWITTER_ACCESS_TOKEN,
+		access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+	});
 
-	passport.use(new TwitterStrategy({
-		consumerKey: TWITTER_CONSUMER_KEY,
-		consumerSecret: TWITTER_CONSUMER_SECRET,
-		callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
-		},
-		function(token,tokenSecret,profile,done){
-			console.log(profile);
-			// twitterProfile = profile;
-			// app.models.User
-			// 	.findOrCreate({
-			// 		'where': {'twtId': profile.id},
-			// 		'defaults':{ 
-			// 			'fullName': profile._json.name,
-			// 			'email': profile._json.email,
-			// 			'active': 1
-			// 		} 
-			// 	})
-			// 	.success(function(localUser, created) {
-			// 		app.models.Twitter
-			// 			.findOrCreate({
-			// 				'where': { 'userId': localUser.id },
-			// 				'defaults':{
-			// 					'id': profile.id,
-			// 					'displayName': profile.displayName,
-			// 					'fullName': localUser.fullName,
-			// 					'gender': profile.gender,
-			// 					'profileUrl': profile.profileUrl,
-			// 					'locale': profile._json.locale,
-			// 					'timezone': profile._json.timezone,
-			// 					'email': profile._json.email,
-			// 					'userId': localUser.id
-			// 				} 
-			// 			})
-			// 			.success(function(user, created) {
-			// 				done(null,localUser);
-			// 			});
-			// 	});
-		}
-	));
+	app.get('/auth/twitter', function(req,res){
+		var screenName = req.query.sn;
+		var params = {screen_name: screenName};
+		client.get('statuses/user_timeline', params, function(error, tweets, response){
+			if (!error) {
+				console.log(tweets);
+			}
+		});
+	});
 
+	//  Google
 	passport.use(new GooglePlusStrategy({
 		consumerKey: 'GOOGLE_CLIENT_ID',
 		consumerSecret: 'GOOGLE_CLIENT_SECRET',
@@ -169,20 +154,6 @@ module.exports = function(){
 			}else{
 				res.redirect('/dash');
 			}
-		}
-	);
-
-
-	// Twitter
-	//	Define Twitter login route
-	app.get('/auth/twitter', passport.authenticate('twitter'));
-
-	//	Define Twitter callback method
-	app.get('/auth/twitter/callback', 
-		passport.authenticate('twitter', { failureRedirect: '/' }),
-		function(req, res) {
-			// Successful authentication
-			console.log('Successful twitter authentication');
 		}
 	);
 
