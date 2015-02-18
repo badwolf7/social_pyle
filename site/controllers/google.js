a2c26f4aabcf6304bb58b57afa7728ef9cf7bb26
Holly Springsteen <hhspringsteen@gmail.com>
2015-02-17 21:27:32 -0500

    better google connections for more data
    attempt at plus domains has failed

--- site/controllers/google.js
+++ site/controllers/google.js
@@ -2,12 +2,16 @@
 var GoogleStrategy = require('passport-google-oauth2');
 var google = require('googleapis');
 var plus = google.plus('v1');
+var plusDomains = google.plusDomains('v1');
 var OAuth2 = google.auth.OAuth2;
 
 // Google App Data
 var GOOGLE_CLIENT_ID = '1044666733227-4u3rtup7s601l651q0ntti5itnpfr1h0.apps.googleusercontent.com';
 var GOOGLE_CLIENT_EMAIL = '1044666733227-4u3rtup7s601l651q0ntti5itnpfr1h0@developer.gserviceaccount.com';
 var GOOGLE_CLIENT_SECRET = 'FEMCRgUb8rlz76wg-jpYlRZp';
+var GOOGLE_API_KEY = 'AIzaSyDHR0JyuKfK58z0K1JHoQc0y8IG-HBCdUw';
+
+var visiblePeople = ['me'];
 
 var oauth2Client = new OAuth2(GOOGLE_CLIENT_ID,GOOGLE_CLIENT_SECRET,'/google/people/get');
 
@@ -16,7 +20,7 @@ module.exports = function(){
 		function(req, res){
 			console.log('');
 			console.log('');
-			console.log('|||||||||||||||||||||  /google/people/get');
+			console.log('|||||||||||||||||||||  google.user');
 			oauth2Client.setCredentials({
 				access_token: req.session.accounts.google.accessToken,
 				refresh_token: req.session.accounts.google.refreshToken
@@ -39,7 +43,7 @@ module.exports = function(){
 			req.session.accounts.google.people = {};
 			console.log('');
 			console.log('');
-			console.log('|||||||||||||||||||||  /google/people/connected/list');
+			console.log('|||||||||||||||||||||  google.people.connected');
 			// collection:  Allowed values: [connected, visible]
 			plus.people.list({userId: 'me', collection: 'connected', auth: oauth2Client}, function(err,response){
 				if(!err){
@@ -57,69 +61,118 @@ module.exports = function(){
 			// List friends that are visible
 			console.log('');
 			console.log('');
-			console.log('|||||||||||||||||||||  /google/people/visible/list');
+			console.log('|||||||||||||||||||||  google.people.visible');
 			// collection:  Allowed values: [connected, visible]
 			plus.people.list({userId: 'me', collection: 'visible', auth: oauth2Client}, function(err,response){
 				if(!err){
 					console.log(response);
 					req.session.accounts.google.people.visible = response;
-					res.redirect('/google/people/activities/list');
+
+					for(var i=0;i<response.length;i++){
+						visiblePeople.push(response[i].id);
+					}
+
+					res.redirect('/google/activities/list');
 				}else{
 					console.log(err);
 				}
 			});
 		}
 	);
-	app.get( '/google/people/activities/list',
+	app.get( '/google/activities/list',
 		function(req, res){
 			// List friends that are online
 			req.session.accounts.google.activities = {};
 			console.log('');
 			console.log('');
-			console.log('|||||||||||||||||||||  /google/activities');
+			console.log('|||||||||||||||||||||  google.activities');
 			// collection:  Allowed values: [connected, visible]
 			plus.activities.list({userId: 'me', collection: 'public', auth: oauth2Client}, function(err,response){
 				if(!err){
 					console.log(response);
+					console.log('');
+					console.log(response.items);
+					console.log('');
+					console.log(response.items[0].object);
+					console.log('');
+					console.log(response.items[0].object.attachments);
+					console.log('');
 					req.session.accounts.google.activities = response;
-					res.redirect('/google/people/moments/list');
+					res.redirect('/google/moments/list');
 				}else{
 					console.log(err);
 				}
 			});
 		}
 	);
-	app.get( '/google/people/comments/list',
+	app.get( '/google/comments/list',
 		function(req, res){
 			// List comments for a specific activity
 			req.session.accounts.google.comments = {};
 			console.log('');
 			console.log('');
-			console.log('|||||||||||||||||||||  /google/people/comments');
+			console.log('|||||||||||||||||||||  google.comments');
 			
 			plus.comments.list({activityId: 'ACTIVITY ID', auth: oauth2Client}, function(err,response){
 				if(!err){
 					console.log(response);
 					req.session.accounts.google.comments = response;
-					res.redirect('/google/people/moments/list');
+					res.redirect('/google/moments/list');
 				}else{
 					console.log(err);
 				}
 			});
 		}
 	);
-	app.get( '/google/people/moments/list',
+	app.get( '/google/moments/list',
 		function(req, res){
 			// List of moments
 			req.session.accounts.google.moments = {};
 			console.log('');
 			console.log('');
-			console.log('|||||||||||||||||||||  /google/people/moments');
+			console.log('|||||||||||||||||||||  google.moments');
 			// collection:  Allowed values: [vault]
 			plus.moments.list({userId: 'me', collection: 'vault', auth: oauth2Client}, function(err,response){
 				if(!err){
 					console.log(response);
 					req.session.accounts.google.moments = response;
+					res.redirect('/dash');
+				}else{
+					console.log(err);
+				}
+			});
+		}
+	);
+	app.get('/google/audiences/list',
+		function(req,res){
+			// List of moments
+			req.session.accounts.google.audiences = {};
+			console.log('');
+			console.log('');
+			console.log('|||||||||||||||||||||  google.audiences');
+
+			plusDomains.audiences.list({userId: 'me', key: GOOGLE_API_KEY}, function(err,response){
+				if(!err){
+					console.log(response);
+					req.session.accounts.google.audiences = response;
+					res.redirect('/google/circles/list');
+				}else{
+					console.log(err);
+				}
+			});
+		}
+	);
+	app.get('/google/circles/list',
+		function(req,res){
+			// List of circles
+			req.session.accounts.google.circles = {};
+			console.log('');
+			console.log('');
+			console.log('|||||||||||||||||||||  google.circles');
+			plusDomains.circles.list({userId: 'me', key: GOOGLE_API_KEY}, function(err,response){
+				if(!err){
+					console.log(response);
+					req.session.accounts.google.circles = response;
 					res.redirect('/dash');
 				}else{
 					console.log(err);
